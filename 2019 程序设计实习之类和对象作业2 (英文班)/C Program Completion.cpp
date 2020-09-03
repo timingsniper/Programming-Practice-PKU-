ad12c28af688c06e38360a1a7c9e3adc6fa3461f
Joonwoo Jang (Percy) <joonwoo_jang@pku.edu.cn>
2020-09-04 00:04:18 +0900

    Add files via upload

---
+++ 2019 程序设计实习之类和对象作业2 (英文班)/C Program Completion.cpp
@@ -0,0 +1,38 @@
+﻿// ConsoleApplication.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include <string>
+#include <cstdio>
+#include <cstring>
+#include <sstream>
+#include <cstdlib>
+using namespace std;
+
+class A {
+public:
+	int val;
+
+	// Your Code Here
+	A(int a = 123) {
+		val = a;
+	}
+
+		
+	A& GetObj() {
+		return *this;
+	}
+};
+int main()
+{
+	int m, n;
+	A a;
+	cout << a.val << endl; //123
+	while (cin >> m >> n) {
+		a.GetObj() = m; // object = int, constructor is called
+		cout << a.val << endl;
+		a.GetObj() = A(n);
+		cout << a.val << endl;
+	}
+	return 0;
+}
---
+++ 2019 程序设计实习之类和对象作业2 (英文班)/D Program Completion2_Complex.cpp
@@ -0,0 +1,46 @@
+﻿// ConsoleApplication.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include <string>
+#include <cstdio>
+#include <cstring>
+#include <sstream>
+#include <cstdlib>
+using namespace std;
+
+class Complex {
+private:
+    double r, i;
+public:
+    void Print() {
+        cout << r << "+" << i << "i" << endl;
+    }
+
+    Complex& operator=(const string& a) {
+        this->r = 0; this->i = 0;
+        bool plusReached = false;
+
+        for (int count = 0; count < a.length(); count++) {
+            if (a[count] == '+') {
+                plusReached = true;
+                continue;
+            }
+            else if (a[count] >= '0' && a[count] <= '9') {
+                if (plusReached) {
+                    this->i = i * 10 + a[count] - '0'; //converting string value num into int
+                }
+                else if (!plusReached) {
+                    this->r = r * 10 + a[count] - '0';
+                }
+            }
+        }
+        return *this;
+   }
+};
+int main() {
+    Complex a;
+    a = "3+4i"; a.Print();
+    a = "5+6i"; a.Print();
+    return 0;
+}
