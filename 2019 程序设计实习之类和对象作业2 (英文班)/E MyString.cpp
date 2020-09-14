6aa5ba1cf40d1af6f8c82410a59290e2f03cc1a1
Joonwoo Jang (Percy) <joonwoo_jang@pku.edu.cn>
2020-09-14 20:39:14 +0900

    Add files via upload

---
+++ 2019 程序设计实习之类和对象作业2 (英文班)/E MyString.cpp
@@ -0,0 +1,66 @@
+﻿// ConsoleApplication.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include <string>
+#include <cstdio>
+#include <cstring>
+#include <sstream>
+#include <cstdlib>
+using namespace std;
+
+class MyString {
+	char* p;
+public:
+	MyString(const char* s) {
+		if (s) {
+			p = new char[strlen(s) + 1];
+			strcpy(p, s);
+		}
+		else
+			p = NULL;
+
+	}
+	~MyString() { if (p) delete[] p; }
+	// Your Code Here
+	friend ostream& operator<<(ostream& output, MyString& a) {
+		output << a.p;
+		return output;
+	}
+
+	void Copy(char* tt) {
+		p = new char[strlen(tt) + 1];
+		strcpy(p, tt);
+	}
+
+	MyString& operator =(MyString& a) {
+		p = new char[strlen(a.p) + 1];
+		strcpy(p, a.p);
+		return *this;
+	}
+
+	MyString& operator =(char *a) {
+		p = new char[strlen(a) + 1];
+		strcpy(p, a);
+		return *this;
+	}
+
+
+
+};
+int main()
+{
+	char w1[200], w2[100];
+	while (cin >> w1 >> w2) {
+		MyString s1(w1), s2 = s1;
+		MyString s3(NULL);
+		s3.Copy(w1);
+		cout << s1 << "," << s2 << "," << s3 << endl;
+
+		s2 = w2;
+		s3 = s2;
+		s1 = s3;
+		cout << s1 << "," << s2 << "," << s3 << endl;
+
+	}
+}
---
+++ 2019 程序设计实习之类和对象作业2 (英文班)/F Big & Base.cpp
@@ -0,0 +1,38 @@
+﻿// ConsoleApplication.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include <string>
+#include <cstdio>
+#include <cstring>
+#include <sstream>
+#include <cstdlib>
+using namespace std;
+
+class Base {
+public:
+	int k;
+	Base(int n) :k(n) { }
+};
+class Big
+{
+public:
+	int v;
+	Base b;
+	// Your Code Here
+
+	Big(int n) : v(n), b(n) {};
+
+
+
+};
+int main()
+{
+	int n;
+	while (cin >> n) { //3
+		Big a1(n); //a1 = 3
+		Big a2 = a1; //a2 = 3
+		cout << a1.v << "," << a1.b.k << endl;
+		cout << a2.v << "," << a2.b.k << endl;
+	}
+}
