d8b6e0e503ad978afd54955657573b659c05b7cc
Joonwoo Jang (Percy) <joonwoo_jang@pku.edu.cn>
2020-09-14 21:28:22 +0900

    Add files via upload

---
+++ 2019 程序设计实习之运算符重载 (英文班)/A Operator Overload.cpp
@@ -0,0 +1,44 @@
+﻿// ConsoleApplication.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include <string>
+#include <cstdio>
+#include <cstring>
+#include <sstream>
+#include <cstdlib>
+using namespace std;
+
+class MyInt
+{
+	int nVal;
+public:
+	MyInt(int n) { nVal = n; }
+	// Your Code Here
+
+	//override int format
+	operator int() {
+		return nVal;
+	}
+
+	//override -
+	MyInt& operator -(int a) {
+		nVal = nVal - a;
+		return *this;
+	}
+};
+int Inc(int n) {
+	return n + 1;
+}
+int main() {
+	int n;
+	while (cin >> n) { //20
+		MyInt objInt(n); //20
+		objInt - 2 - 1 - 3;
+		cout << Inc(objInt);
+		cout << ",";
+		objInt - 2 - 1;
+		cout << Inc(objInt) << endl;
+	}
+	return 0;
+}
---
+++ 2019 程序设计实习之运算符重载 (英文班)/B Amazing ip and op of point.cpp
@@ -0,0 +1,40 @@
+﻿// ConsoleApplication.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include <string>
+#include <cstdio>
+#include <cstring>
+#include <sstream>
+#include <cstdlib>
+using namespace std;
+
+class Point {
+private:
+	int x;
+	int y;
+public:
+	Point() { };
+	// Your Code Here
+
+	//input overload
+	friend istream& operator >>(istream &input, Point &a) {
+		input >> a.x >> a.y;
+		return input;
+	}
+
+	//cout overload
+	friend ostream& operator<<(ostream& output, Point& b) {
+		output << b.x << "," << b.y;
+		return output;
+	}
+
+};
+int main()
+{
+	Point p;
+	while (cin >> p) {
+		cout << p << endl;
+	}
+	return 0;
+}
